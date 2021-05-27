c8865ccd530ec3e8d3705e833e5438d1dc77fe78
lapohjan <59822237+lapohjan@users.noreply.github.com>
2021-05-27 10:35:16 +0300

    set up basic travel time calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [speed, setSpeed] = useState(0);
+  // const [car, setCar] = useState(0);
+  const [distance, setDistance] = useState(0);
+  // const [fuelConsumption, setFuelConsumption] = useState(0);
+  const [travelTime, setTravelTime] = useState(0);
+
+  const calculateTravelTime = (e) => {
+    e.preventDefault();
+    const formValid = speed > 0 && distance > 0;
+    if (!formValid) {
+      return;
+    }
+    const time = distance / speed;
+    const n = new Date(0, 0);
+    n.setMinutes(+time * 60);
+    const travelTime = n.toTimeString().slice(0, 5);
+    setTravelTime(travelTime);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={calculateTravelTime}>
+        <div>
+          <label>Speed in km / h</label>
+          <input value={speed} onChange={(e) => setSpeed(e.target.value)} />
+        </div>
+
+        <div>
+          <label>Distance in km</label>
+          <input
+            value={distance}
+            onChange={(e) => setDistance(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">calculate</button>
+      </form>
+      <p>Travel time: {travelTime}</p>
     </div>
   );
 }
